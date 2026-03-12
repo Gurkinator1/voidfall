f45bb33b762dc8b2a86c960a0cc599d9cff35ed8
Qunkus <gh@roeskefelix.de>
2026-03-12 21:58:59 +0100

    feat: changed player animation logic

--- scripts/player_controller.gd
+++ scripts/player_controller.gd
@@ -5,25 +5,18 @@ extends CharacterBody2D
 
 const SPEED = 350.0
 
-func get_input():
-	var input_direction = Input.get_vector("left", "right", "up", "down")
-	velocity = input_direction * SPEED
-
-func walking_animation():
-	if Input.is_action_pressed("left"):
-		_animated_sprite.flip_h = false
-		_animated_sprite.play("walking")
-	elif Input.is_action_pressed("right"):
-		_animated_sprite.flip_h = true
-		_animated_sprite.play("walking")
-	elif Input.is_action_pressed("up") or Input.is_action_pressed("down"):
+func update_animation(input_direction: Vector2):
+	if input_direction.length()>0 :
 		_animated_sprite.play("walking")
 	else:
 		_animated_sprite.stop()
+	if input_direction.x != 0:
+		_animated_sprite.flip_h = input_direction.x > 0;
 
 func _physics_process(_delta: float) -> void:
-	get_input()
+	var input_direction = Input.get_vector("left", "right", "up", "down")
+	
+	velocity = input_direction * SPEED
 	move_and_slide()
-
-func _process(_delta: float) -> void:
-	walking_animation()
+	
+	update_animation(input_direction)
